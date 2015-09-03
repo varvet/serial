0ac3f3b4f7b4886d41ce075fb22c3020ac497601
Kim Burgestrand <kim@elabs.se>
2015-09-03 11:51:27 +0200

    Begin docs for Serializer.

--- lib/serial/serializer.rb
+++ lib/serial/serializer.rb
@@ -1,18 +1,45 @@
 module Serial
   class Serializer
+    # Create a new Serializer, using the block as instructions.
+    #
+    # @example
+    #   PersonSerializer = Serial::Serializer.new do |h, person|
+    #     h.attribute(:id, person.id)
+    #     h.attribute(:name, person.name)
+    #   end
+    #
+    #   PersonSerializer.call(…)
+    #
+    # @yield [builder, *args]
+    # @yieldparam builder [HashBuilder]
+    # @yieldparam *args
     def initialize(&block)
       @block = block
     end
 
-    def map(context = nil, list)
-      list.map { |item| call(context, item) }
+    # Invoke a Serializer, optionally passing it a context to be evaluated in.
+    #
+    # @example with context, the serializer block is evaluated inside the context
+    #   # app/serializers/person_serializer.rb
+    #   PersonSerializer = Serial::Serializer.new do |h, person|
+    #     h.attribute(:id, person.id)
+    #     h.attribute(:url, people_url(person))
+    #   end
+    #
+    #   # app/controllers/person_controller.rb
+    #   def show
+    #     person = Person.find(…)
+    #     render json: PersonSerializer.call(self, person)
+    #   end
+    #
+    # @example (TODO) without context, the serializer block is evaluated using normal closure rules
+    #
+    def call(context = nil, value)
+      HashBuilder.build(context, value, &@block)
     end
 
-    def call(context = nil, value)
-      block = @block
-      HashBuilder.build(context) do |builder|
-        builder.exec(value, &block)
-      end
+    def map(context = nil, list)
+      list.map { |item| call(context, item) }
     end
 
     def to_proc
