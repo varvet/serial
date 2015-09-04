71d3f2268e53745faec2d5577d0b0730c07f5334
Kim Burgestrand <kim@elabs.se>
2015-09-04 09:50:05 +0200

    Clean up main spec.

--- spec/serial_spec.rb
+++ spec/serial_spec.rb
@@ -1,122 +1,76 @@
-class MyContext
-  def uppercase(value)
-    value.to_s.upcase
-  end
-end
-
-PersonSerializer = Serial::Serializer.new do |h, person|
-  h.attribute(:id, person.id)
-  h.attribute(:name, uppercase(person.name))
-end
-
-ProjectSerializer = Serial::Serializer.new do |h, project|
-  h.attribute(:id, project.id)
-  h.attribute(:projectName, uppercase(project.name))
-  h.attribute(:description, project.description)
-
-  h.attribute(:client, project.client) do |h, client|
-    h.attribute(:id, client.id)
-    h.attribute(:name, client.name)
-  end
-
-  h.map(:people, project.people, &PersonSerializer)
-
-  h.map(:assignments, project.assignments) do |h, assignment|
-    h.attribute(:id, assignment.id)
-    h.attribute(:duration, assignment.duration)
-
-    h.attribute(:person, assignment.person, &PersonSerializer)
+class FakeContext
+  def dr_prefix(name)
+    "Dr. #{name}"
   end
 end
 
 describe Serial do
-  let(:kim) { double({ id: 1, name: "Kim" }) }
-  let(:jonas) { double({ id: 2, name: "Jonas" }) }
-
-  let(:project) do
-    double({
-      id: 13,
-      name: "ProjectPuzzle",
-      description: "ProjectPuzzle is our own product",
-      client: double({
-        id: 5,
-        name: "Elabs AB"
-      }),
-      people: [kim, jonas],
-      assignments: [
-        double({ id: 1, duration: "forever", person: kim }),
-        double({ id: 2, duration: "forever ever", person: jonas }),
-      ]
-    })
+  def dr_prefix(name)
+    "Doctor #{name}"
   end
 
-  it 'has a version number' do
+  let(:person_with_friend) do
+    double(name: "Kim", friend: double(name: "Jonas", friend: nil))
+  end
+
+  let(:other_person_with_friend) do
+    double(name: "Piff", friend: double(name: "Puff", friend: nil))
+  end
+
+  let(:friend_serializer) do
+    this_serializer = Serial::Serializer.new do |h, person|
+      h.attribute(:name, dr_prefix(person.name))
+      h.attribute(:friend, person.friend, &this_serializer) if person.friend
+    end
+  end
+
+  it "has a version number" do
     expect(Serial::VERSION).not_to be nil
   end
 
-  def prefix(value)
-    "Mr. #{value}"
-  end
-
-  it 'works without context object' do
-    person_serializer = Serial::Serializer.new do |h, person|
-      h.attribute(:id, person.id)
-      h.attribute(:name, prefix(person.name))
+  describe "#call" do
+    specify "without context the serializer is executed inside the context" do
+      expect(friend_serializer.call(person_with_friend)).to eq({
+        "name" => "Doctor Kim",
+        "friend" => { "name" => "Doctor Jonas" }
+      })
     end
 
-    full_person_serializer = Serial::Serializer.new do |h, person|
-      h.attribute(:id, person.id)
-      h.attribute(:name, prefix(person.name))
-      h.attribute(:friend, jonas, &person_serializer)
+    specify "with context the serializer is executed inside the context" do
+      expect(friend_serializer.call(FakeContext.new, person_with_friend)).to eq({
+        "name" => "Dr. Kim",
+        "friend" => { "name" => "Dr. Jonas" }
+      })
     end
-
-    expect(full_person_serializer.call(kim)).to eq({
-      "id" => 1,
-      "name" => "Mr. Kim",
-      "friend" => {
-        "id" => 2,
-        "name" => "Mr. Jonas",
-      }
-    })
   end
 
-  it 'works with context object' do
-    expect(ProjectSerializer.call(MyContext.new, project)).to eq({
-      "id" => 13,
-      "projectName" => "PROJECTPUZZLE",
-      "description" => "ProjectPuzzle is our own product",
-      "client" => {
-        "id" => 5,
-        "name" => "Elabs AB"
-      },
-      "people" => [
+  describe "#map" do
+    let(:people) { [person_with_friend, other_person_with_friend] }
+
+    specify "without context the serializer is executed inside the context" do
+      expect(friend_serializer.map(people)).to eq([
         {
-          "id" => 1,
-          "name" => "KIM"
+          "name" => "Doctor Kim",
+          "friend" => { "name" => "Doctor Jonas" }
         },
         {
-          "id" => 2,
-          "name" => "JONAS"
-        }
-      ],
-      "assignments" => [
+          "name" => "Doctor Piff",
+          "friend" => { "name" => "Doctor Puff" }
+        },
+      ])
+    end
+
+    specify "with context the serializer is executed inside the context" do
+      expect(friend_serializer.map(FakeContext.new, people)).to eq([
         {
-          "id" => 1,
-          "duration" => "forever",
-          "person" => {
-            "id" => 1,
-            "name" => "KIM"
-          }
+          "name" => "Dr. Kim",
+          "friend" => { "name" => "Dr. Jonas" }
         },
         {
-          "id" => 2,
-          "duration" => "forever ever",
-          "person" => {
-            "id" => 2,
-            "name" => "JONAS"
-          }
-        }
-      ]
-    })
+          "name" => "Dr. Piff",
+          "friend" => { "name" => "Dr. Puff" }
+        },
+      ])
+    end
   end
 end
